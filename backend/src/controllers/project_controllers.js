aacdf90f1119856bd5d47ddb0e682901b1dec18c
ayushks404 <ayushks9415@gmail.com>
2026-01-17 14:18:08 +0530

    added api to fetch project data at frontend

--- backend/src/controllers/project_controllers.js
+++ backend/src/controllers/project_controllers.js
@@ -37,9 +37,39 @@ export const createproject = async (req, res) => {
 
     res.status(201).json({ project });
 
-  } catch (err) {
+  }
+  catch (err) {
     console.error("Create project error:", err);
     res.status(500).json({ message: err.message });
   }
 };
 
+
+
+
+
+
+export const getProjectById = async (req, res) => {
+  
+  try {
+    const project = await Project.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    res.json({
+      name: project.name,
+      repourl: project.repourl,
+      indexed: project.index,
+    });
+
+  }
+  catch (err) {
+    console.error("Get project error:", err);
+    res.status(500).json({ message: err.message });
+  }
+};
